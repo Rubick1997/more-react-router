456109dfd49cb707037d09db121fbd5b2cb7dbd8
Rustam Kolumbayev <rustamkolumbayev@Rustams-MacBook-Pro.local>
2021-06-01 17:10:56 -0700

    switch and exact

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {useParams} from "react-router-dom"
+
+function ProductDetail() {
+    const params = useParams()
+
+    return (
+        <section>
+            <h1>Product Detail</h1>
+        </section>
+    )
+}
+
+export default ProductDetail
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Products() {
-    return (
-        <div>
-            <h1>Products page</h1>
-        </div>
-    )
+  return (
+    <section>
+      <h1>Products page</h1>
+      <ul>
+        <li>
+          <Link to="/products/p1">Book</Link>
+        </li>
+        <li>
+          <Link to="/products/p2">Carpet</Link>
+        </li>
+        <li>
+          <Link to="/products/p3">Movie </Link>
+        </li>
+      </ul>
+    </section>
+  );
 }
 
-export default Products
+export default Products;
